0236bf0a94c746b882c206bc98f7d2d96b2d02c8
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2023-11-04 18:56:16 +0530

    temp html

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,24 +1,34 @@
+import React from "react";
+import { ReactDOM } from "react";
+import "./Assets/temp-img.png";
+import { AiFillMessage, AiFillPhone, AiFillSetting, AiOutlineLeft, AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
 function Sidebar() {
-  return ( 
+  return (
     <div>
-      <navbar>
-        <ul>
-          <li><a href="#">CoGro</a></li>
-          <li><a href="#">Search</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">Profile</a></li>
+      <navbar className="sidebar">
+        <div className="side-list">
+          <a href="#"><img src={require('./Assets/temp-img.png')} /> <span>Profile</span></a>
+          <ul className="side-links">
+            <li><a href="#"><img src={require('./Assets/temp-img.png')} /> CoGro</a></li>
+            <li><a href="#"><AiOutlineSearch /> <span>Search</span></a></li>
+            <li><a href="#"><AiFillMessage /> <span>Inbox</span></a></li>
+            <li><a href="#"><AiFillPhone /> <span>Contact</span></a></li>
+          </ul>
+        </div>
 
-        </ul>
+        <div className="side-low">
+          <span className="sidebar-expand"><AiOutlineRight /></span>
+          <span className="sidebar-Collapse"><AiOutlineLeft /></span>
+          <ul className="side-links" id="below-links">
+            
+            <li><a href="#"><AiFillSetting /> <span>Settings</span></a></li>
+          </ul>
+        </div>
 
-        <ul>
-          <li><a href="#">CoGro</a></li>
-          <li><a href="#">Search</a></li>
-          <li><a href="#">Contact</a></li>
 
-        </ul>
       </navbar>
     </div>
-   );
+  );
 }
 
 export default Sidebar;
