c8d14af54cefde20d7e6f3d86b941ee90a5a2393
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2023-11-04 23:15:14 +0530

    sidebar complete

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { ReactDOM } from "react";
 import "./Assets/temp-img.png";
-import { AiFillMessage, AiFillPhone, AiFillSetting, AiOutlineLeft, AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
+import { AiFillInfoCircle, AiFillMessage, AiFillPhone, AiFillSetting, AiOutlineLeft, AiOutlineRight, AiOutlineSearch } from "react-icons/ai";
+
 function Sidebar() {
+
   return (
     <div>
       <navbar className="sidebar">
         <div className="side-list">
           <ul className="side-links">
-            <li id="side-title"><a href="#"><img src={require('./Assets/cogro-logo.png')} /> <span>CoGro</span></a></li>
+            <li id="side-title"><a href="#"><img className="side-logo" src={require('./Assets/cogro-logo.png')} /> <span className="cogro-title">CoGro</span><span id="sd-slogan" style={{fontSize: "0.8rem"}}>Collab, Connect, Grow..!</span></a></li>
             <li><a href="#"><AiOutlineSearch /> <span id="sl-label">Search</span></a></li>
-            <li><a href="#"><AiFillMessage /> <span id="sl-label">Inbox</span></a></li>
             <li><a href="#"><AiFillPhone /> <span id="sl-label">Contact</span></a></li>
+            <li><a href="#"><AiFillInfoCircle /> <span id="sl-label">About us</span></a></li>
+            <li><a href="#"><AiFillSetting /> <span id="sl-label">Settings</span></a></li>
           </ul>
         </div>
 
         <div className="side-low">
           <a href="#"><div className="profile-side-mein"><img className="side-profile" src={require('./Assets/temp-img.png')} /> <div className="profile-name"><abbr title="Your Instagram Username"><span id="sd-username">@username</span></abbr></div></div></a>
-          <span className="sd-arr sidebar-expand"><AiOutlineRight /></span>
-          <span className="sd-arr sidebar-Collapse"><AiOutlineLeft /></span>
+          
           <ul className="side-links" id="below-links">
-            <li><a href="#"><AiFillSetting /> <span id="sl-label">Settings</span></a></li>
           </ul>
         </div>
-
 
       </navbar>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import { Route, Link, BrowserRouter as Router, Routes } from "react-router-dom"
+
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routing = (
+  <Router>
+    <Routes>
+      <Route path="/" Component={App} />
+      {/* <Route path="/about" Component={About}/>
+          <Route path="/contact" Component={Contact}/> */}
+    </Routes>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'))
